2f31b9af9ed02eac423e5235177215cb7384e560
websmaniacdev <websmaniacdev@gmail.com>
2021-04-16 10:30:56 +0530

    Initial Commit

---
+++ src/index.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace websmaniac\calculate;
+
+class Index
+{
+    public function addNumbers($a = 0, $b = 0)
+    {
+        return $a+$b;
+    }
+
+    public function subNumbers($a = 0, $b = 0)
+    {
+        return $a-$b;
+    }
+
+    public function multiNumbers($a = 1, $b = 1)
+    {
+        return $a*$b;
+    }
+
+    public function divNumbers($a = 1, $b = 1)
+    {
+        return $a*$b;
+    }
+
+}
